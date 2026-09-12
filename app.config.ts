86218bea324c89864c3d8058abdb099029b68b03
Zakariasalah1996 <2.76284296e+08+Zakariasalah1996@users.noreply.github.com>
2026-09-12 12:03:47 +0000

    Prepare Android 1.0.89 APK

--- app.config.ts
+++ app.config.ts
@@ -43,7 +43,7 @@ const env = {
 const config: ExpoConfig = {
   name: env.appName,
   slug: env.appSlug,
-  version: isEasIosBuild ? "1.0.61" : "1.0.88",
+  version: isEasIosBuild ? "1.0.61" : "1.0.89",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme: env.scheme,
@@ -67,7 +67,7 @@ const config: ExpoConfig = {
     edgeToEdgeEnabled: true,
     predictiveBackGestureEnabled: false,
     package: env.androidPackage,
-    versionCode: 10067,
+    versionCode: 10068,
     googleServicesFile: "./google-services.json",
     permissions: [
       "POST_NOTIFICATIONS",
