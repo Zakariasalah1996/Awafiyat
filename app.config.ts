7351f724966c6e1787ba460e6d6e7d5d57a6418c
Zakariasalah1996 <2.76284296e+08+Zakariasalah1996@users.noreply.github.com>
2026-09-13 16:08:46 +0000

    chore: bump android release to 1.0.90

--- app.config.ts
+++ app.config.ts
@@ -43,7 +43,7 @@ const env = {
 const config: ExpoConfig = {
   name: env.appName,
   slug: env.appSlug,
-  version: isEasIosBuild ? "1.0.61" : "1.0.89",
+  version: isEasIosBuild ? "1.0.61" : "1.0.90",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme: env.scheme,
@@ -67,7 +67,7 @@ const config: ExpoConfig = {
     edgeToEdgeEnabled: true,
     predictiveBackGestureEnabled: false,
     package: env.androidPackage,
-    versionCode: 10068,
+    versionCode: 10069,
     googleServicesFile: "./google-services.json",
     permissions: [
       "POST_NOTIFICATIONS",
